80ec80b27296301df50c4309d79775a62153a834
Jon Ryan <jon@treefish.uk>
2022-01-29 12:39:23 +0000

    Initial Setup

---
+++ src/LegacyMVCApp/Controllers/LegacyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace LegacyMVCApp.Controllers
+{
+    public class LegacyController : Controller
+    {
+        public LegacyController()
+        {
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Protected()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/NewMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NewMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
