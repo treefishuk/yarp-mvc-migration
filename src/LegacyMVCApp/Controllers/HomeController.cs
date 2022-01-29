5def32481f7306c0a130e6664784e99e6ad63b8c
Jon Ryan <jon@treefish.uk>
2022-01-29 12:43:01 +0000

    Added Info Page to Legacy App

---
+++ src/LegacyMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace LegacyMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }       
+    }
+}
